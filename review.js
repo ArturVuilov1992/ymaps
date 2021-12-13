1580402ce341b659e3c270d6fdf870eb725d320b
ArturVuilov1992 <87204984+ArturVuilov1992@users.noreply.github.com>
2021-12-13 20:59:06 +0300

    ymaps

--- review.js
+++ review.js
@@ -7,46 +7,48 @@ export default class MyReview {
     this.map.init().then(this.onInit.bind(this));
   }
 
-  async onInit() {
-    document.body.addEventListener('click', this.onDocumentClick.bind(this));
+onClick(coords) {
+    //эта функция вызовется только после шелчка по карте, а coords это координаты щелчка
+    const form = this.createForm(coords);
+    // этот this это this.onClick.bind(this)) ????
+    this.map.openBalloon(coords, form.innerHtml);
   }
+  
   createForm(coords, reviews) {
     //это просто форма в воздухе без координатов,coords пока неизвестен, это лишь заготовка
     const root = document.createElement('div');
     root.innerHTML = this.formTemplate;
-    const reviewList = root.querySelector('.review-list');
-    const reviewForm = root.querySelector('[data-role=review-form]');
+    const reviewList = root.querySelector('.review-list');//накопитель
+    const reviewForm = root.querySelector('[data-role=review-form]');// сама форма
+
     reviewForm.dataset.coords = JSON.stringify(coords);
     //добавляем к форме доп.атрибут data-coords,где в виде строки будут координаты
-
     for (const item of reviews) {
       const div = document.createElement('div');
-      div.classList.add('review-item');
-      div.innerHTML = `
+/*       div.classList.add('review-item');
+ */      div.innerHTML = `
       <div>
         <b> ${{item.name}}</b> [${{item.place}}]
       </div>
-      <div> $(item.text) </div>
+      <div> ${{item.text}} </div>
       `;
 reviewList.appendChild(div);
     }
 
     return root;
   }
-  onClick(coords) {
-    //эта функция вызовется только после шелчка по карте, а coords это координаты щелчка
-    const form = this.createForm(coords);
-    // этот this это this.onClick.bind(this)) ????
-    this.map.openBalloon(coords, form.innerHtml);
+
+  async onInit() {
+    document.body.addEventListener('click', this.onDocumentClick.bind(this));
   }
   async onDocumentClick(e) {
     //завязывание на элементы разметки лучше делать через датат атрибуты, так как классы ненадежны и вебпак может их переиначить
     if (e.target.dataset.role === 'review-add') {
-      const reviewForm = document.querySelector('[data-role=review-form]');
+      const reviewForm = document.querySelector('[data-role=review-form]');//сама форма
       const coords = JSON.parse(reviewForm.dataset.coords);
       const data = {
         coords,
-        review: {
+        reviews: {
           name: document.querySelector('[data-role=review-name]').value,
           place: document.querySelector('[data-role=review-place]').value,
           text: document.querySelector('[data-role=review-text]').value,
